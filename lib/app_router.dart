c696090070e7ca8c640d0e1272d39967041d048c
64Book <booknarawich2001@gmail.com>
2024-10-19 16:02:34 +0700

    update

--- lib/app_router.dart
+++ lib/app_router.dart
@@ -1,18 +1,24 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter_store/screens/login/login_screen.dart';
+import 'package:flutter_store/screens/register/register_screen.dart';
 import 'package:flutter_store/screens/welcome/welcome_screen.dart';
+import 'package:flutter_store/screens/forgetpassword/forget_password_screen.dart';
 
 class AppRouter {
 
   // Router Map Key
   static const String welcome = 'welcome';
   static const String login = 'login';
+  static const String register = 'register';
+  static const String forgotPassword = 'forgotPassword';
 
   // Router Map
   static get routes => {
     welcome: (context) => WelcomeScreen(),
     login: (context) => LoginScreen(),
+    register: (context) => RegisterScreen(), 
+    forgotPassword: (context) => ForgotPasswordScreen(),
   };
 
 }
---
+++ lib/utils/constants.dart
@@ -0,0 +1,12 @@
+
+// API URL
+// Local
+const baseURLAPI = 'http://192.168.99.85:3000/api/';
+const baseURLImage = 'http://192.168.99.85:3000/uploads/images/';
+
+// const baseURLAPI = 'http://10.6.109.183:3000/api/';
+// const baseURLImage = 'http://10.6.109.183:3000/uploads/images/';
+
+// Server
+// const baseURLAPI = 'https://serverapi-node.onrender.com/api/';
+// const baseURLImage = 'https://serverapi-node.onrender.com/uploads/images/';
